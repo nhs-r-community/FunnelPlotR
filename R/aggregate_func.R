f4f5ac6336426d475ab2da5ae2affaee3e530e2a
Chris Mainey <chrismainey@gmail.com>
2020-05-19 20:21:02 +0100

    removed dplyr dependencies from aggregate function

--- R/aggregate_func.R
+++ R/aggregate_func.R
@@ -3,18 +3,17 @@
 #' @param mod_plot A data frame of values to be aggregated.  Expected columns, 'group', 'numerator' and 'denominator'
 #' @keywords internal
 #' @return Returns an aggregated data.frame of the same columns, but summed by 'group', with a relative risk 'rr' column added.
-#' @importFrom dplyr group_by summarise %>%
-#' @importFrom rlang .data
 
-aggregate_func<- function(mod_plot) {
+aggregate_func2<- function(mod_plot) {
 
-  mod_plot_agg <- mod_plot %>%
-    dplyr::group_by(.data$group) %>%
-    dplyr::summarise(
-      numerator = as.numeric(sum(.data$numerator)),
-      denominator = as.numeric(sum(.data$denominator)),
-      rr = .data$numerator / .data$denominator
-    )
+ mod_plot_agg <- 
+   data.frame(
+     aggregate(list(numerator =mod_plot$numerator, denominator=mod_plot$denominator)
+               ,  by=list(group = mod_plot$group)
+               , FUN=sum)
+   )
+   
+  mod_plot_agg$rr = mod_plot_agg$numerator/ mod_plot_agg$denominator
 
 return(mod_plot_agg)
 
